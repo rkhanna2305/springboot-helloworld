485ceb18459209452f27ebb0180f4b1585b4b8fa
rkhanna2305 <111944040+rkhanna2305@users.noreply.github.com>
2025-06-09 08:34:27 +0000

    Updated controller to print Hello World five times

--- src/main/java/com/example/helloworld/HelloController.java
+++ src/main/java/com/example/helloworld/HelloController.java
@@ -5,8 +5,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
     @GetMapping("/")
     public String hello() {
-        return "Hello, World!";
+        StringBuilder response = new StringBuilder();
+        for (int i = 0; i < 5; i++) {
+            response.append("Hello World<br>");
+        }
+        return response.toString();
     }
 }
